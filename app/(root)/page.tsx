6e0c6c37f69831b3a27bee67f389abd0c968b32c
LOISGALLAUD <loisgallaud@hotmail.com>
2024-05-12 15:20:35 +0200

    Implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Loïs",
+    lastName: "Gallaud",
+    email: "contact@loisg.pro",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, {}]}
+      />
+    </section>
+  );
 };
 
 export default Home;
